afa4024f66dc2a4cfcd6d012b5e12d0db09343f6
ofakinlede <ofakinlede@gmail.com>
2026-05-13 18:37:50 -0400

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://jkokxmxpx6.execute-api.us-east-1.amazonaws.com/default/lambda1'
+const API_ENDPOINT = 'https://zyks92flm4.execute-api.us-east-1.amazonaws.com/default/lambda1'
 
 export default {
   name: 'app',
